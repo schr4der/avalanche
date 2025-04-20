c8a244ec7c22ffaac2417b5653b589c4c21ed53c
oliox <oliox@students.cs.ubc.ca>
2025-04-20 13:07:10 -0700

    modified UNet model

--- src/dataset.py
+++ src/dataset.py
@@ -65,7 +65,7 @@ class GeoTiffSegmentationDataset(Dataset):
         
         # --- Load stitched image and metadata ---
         image, meta = stitch_tiles(row, col, self.tiff_dir, self.tile_size)
-
+        image = normalize(image)
         # --- Rasterize shapefile to match the image extent ---
         mask = rasterize_shp(self.shapefile, meta)
 
@@ -82,6 +82,10 @@ class GeoTiffSegmentationDataset(Dataset):
         mask_tensor = mask_tensor.squeeze(0)
 
         return img_tensor, mask_tensor
+
+#TODO improve to mean/std dataset stats img = (img - mean) / std
+def normalize(img):
+    return (img - img.min()) / (img.max() - img.min() + 1e-8)
 
 def get_tile_filename(row, col, base_dir):
     pattern = os.path.join(base_dir, f"swissalti3d_*_{row}-{col}_2_2056_5728.tif")
--- src/visualize_results.py
+++ src/visualize_results.py
@@ -2,6 +2,7 @@ import torch
 import matplotlib.pyplot as plt
 from dataset import GeoTiffSegmentationDataset
 from simpleModel import SimpleFCN  # or UNet if you're using that
+from uNetSmaller import UNet_Modified
 from torch.utils.data import random_split
 
 def visualize_prediction():
@@ -16,23 +17,23 @@ def visualize_prediction():
     test_dataset, val_dataset = random_split(test_dataset, [0.5, 0.5], generator=generator)
 
     # Load model and checkpoint
-    model = SimpleFCN(in_channels=1, num_classes=1).to(device)
+    model = UNet_Modified(in_channels=1, num_classes=1).to(device)
     model.load_state_dict(torch.load('my_checkpoint.pth', map_location=device))
     model.eval()
 
     # Pick one sample from the test dataset
-    img, true_mask = test_dataset[10]
+    img, true_mask = test_dataset[50]
+    print(img)
     train_img, train_mask = train_dataset[19]
-    print("Mask min/max:", true_mask.min(), true_mask.max())
-    train_img = train_img.unsqueeze(0).float().to(device)  # add batch dimension
+    img = img.unsqueeze(0).float().to(device)  # add batch dimension
 
     with torch.no_grad():
-        pred_mask = model(train_img)
+        pred_mask = model(img)
         pred_mask = torch.sigmoid(pred_mask)
         pred_mask = pred_mask.squeeze().cpu().numpy()
     
-    train_img = train_img.squeeze().cpu().numpy()
-    train_mask = train_mask.numpy()
+    img = img.squeeze().cpu().numpy()
+    true_mask = true_mask.numpy()
 
     plt.imshow(pred_mask.squeeze(), cmap='viridis')
     plt.title("Raw Sigmoid Output")
@@ -41,12 +42,12 @@ def visualize_prediction():
     plt.figure(figsize=(12, 4))
     
     plt.subplot(1, 3, 1)
-    plt.imshow(train_img, cmap='gray')
+    plt.imshow(img, cmap='gray')
     plt.title("Input Image")
     plt.axis('off')
 
     plt.subplot(1, 3, 2)
-    plt.imshow(train_mask, cmap='gray')
+    plt.imshow(true_mask, cmap='gray')
     plt.title("Ground Truth Mask")
     plt.axis('off')
 
